df6a6af1a06eb32747d1b90d122a46420f841b54
GabRac <gracine50@gmail.com>
2023-01-11 18:34:47 -0500

    Ajout de la scène Enemy

---
+++ Enemy.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var speed = 10
+var motion = Vector2.ZERO
+var screen_size = get_viewport_rect().size
+
+onready var player = get_node("res://Player.tscn") 
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass
+	
+func _physics_process(delta):
+
+	motion += position.direction_to(player.position)
+	
+	look_at(player.position)
+	
+	motion = move_and_slide(motion)
