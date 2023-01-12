a0df3671b41fec4aa4de6c27b61d8b16f7f71769
Malyk <malykoum@gmail.com>
2023-01-12 01:27:01 -0500

    playernothiting

--- Enemy.gd
+++ Enemy.gd
@@ -4,9 +4,9 @@ extends KinematicBody2D
 var damage = 10
 export var dammage = 1
 var motion = Vector2.ZERO
-var screen_size = get_viewport_rect().size
+#var screen_size = get_viewport_rect().size
 
-onready var player = get_node("res://Player.tscn") 
+#onready var player = get_node("res://Player.tscn") 
 
 enum {
 	Attack,
@@ -22,14 +22,18 @@ var velocity = Vector2.ZERO
 var threshold = .1
 var turn_speed = 10
 
+var health = 20
+
 onready var nav = get_parent()
 onready var raycast = $RayCast2D
+
+var player : KinematicBody2D
 
 func move_to(_location):
 	var _target = _location
 
 func _ready():
-	screen_size = get_viewport_rect().size
+#	screen_size = get_viewport_rect().size
 	pass
 	
 func _physics_process(delta):
@@ -45,10 +49,10 @@ func _process(delta):
 			rotate(deg2rad($Look.rotation * turn_speed))
 			if raycast.is_colliding():
 				var collision = raycast.get_collider()
-				print(collision)
+				#print(collision)
 				if collision.is_in_group("Player"):
 					collision.health -= dammage
-					print(collision.health)
+					#print(collision.health)
 			
 
 func move_to_target():
@@ -62,5 +66,15 @@ func move_to_target():
 		
 func get_target_path(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
-	
-	
+
+
+func _on_Enemy_area_body_entered(body):
+	if body.name == "Player":
+		player = body as Player
+		print(player.name)
+		var isAttacked = player.get_readyAttack()
+		if isAttacked:
+			health -= 10
+			player.set_readyAttack(0)
+			print("As been hit")
+		pass
