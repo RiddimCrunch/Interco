3d632248b06672b45d9e53b47f6e6dbb46fe0b99
Samuel Cossette <1957527@cshawi.ca>
2023-01-12 01:51:58 -0500

    Test Fix

--- Enemy.gd
+++ Enemy.gd
@@ -4,9 +4,9 @@ extends KinematicBody2D
 var damage = 10
 export var dammage = 1
 var motion = Vector2.ZERO
-#var screen_size = get_viewport_rect().size
+var screen_size = get_viewport_rect().size
 
-#onready var player = get_node("res://Player.tscn") 
+onready var player = get_node("res://Player.tscn") 
 
 enum {
 	Attack,
@@ -22,21 +22,14 @@ var velocity = Vector2.ZERO
 var threshold = .1
 var turn_speed = 10
 
-var health = 20
-
 onready var nav = get_parent()
 onready var raycast = $RayCast2D
-
-var player : KinematicBody2D
 
 func move_to(_location):
 	var _target = _location
 
 func _ready():
-<<<<<<< HEAD
-=======
-#	screen_size = get_viewport_rect().size
->>>>>>> e67ebdb729e38d6b595595a2d3e824e472c9d681
+	screen_size = get_viewport_rect().size
 	pass
 	
 func _physics_process(delta):
@@ -46,25 +39,17 @@ func _physics_process(delta):
 func _process(delta):
 	match current_state:
 		Attack:
-			$AnimationPlayer.play("Attack")
+			pass
 		Walk:
-			$AnimationPlayer.play("Walk")
 			$Look.look_at(Player.global_transform.origin)
 			rotate(deg2rad($Look.rotation * turn_speed))
 			if raycast.is_colliding():
 				var collision = raycast.get_collider()
-<<<<<<< HEAD
-				#print(collision)
-=======
->>>>>>> 882bf63e75c5343099e8d44a043378729bf1c495
+				print(collision)
 				if collision.is_in_group("Player"):
-					current_state = Attack
 					collision.health -= dammage
-					
-					$Timer.start()
-					if ($Timer.time_left > 0):
-						speed = 100
-					#print(collision.health)
+					print(collision.health)
+			
 
 func move_to_target():
 	if global_transform.origin.distance_to(path[cur_path_idx]) < threshold:
@@ -77,30 +62,5 @@ func move_to_target():
 		
 func get_target_path(target_pos):
 	path = nav.get_simple_path(global_transform.origin, target_pos)
-<<<<<<< HEAD
-
-
-func _on_Enemy_area_body_entered(body):
-	if body.name == "Player":
-		player = body as Player
-		print(player.name)
-		var isAttacked = player.get_readyAttack()
-		if isAttacked:
-			health -= 10
-			player.set_readyAttack(0)
-			print("As been hit")
-		pass
-=======
 	
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "Attack":
-		current_state = Walk
-<<<<<<< HEAD
-
-
-func _on_Timer_timeout():
-	speed = 350
-=======
->>>>>>> 882bf63e75c5343099e8d44a043378729bf1c495
->>>>>>> e67ebdb729e38d6b595595a2d3e824e472c9d681
+	
