e02df5f6916371a6fd0d94ff9f010a300bfaae4b
Samuel Cossette <1957527@cshawi.ca>
2023-01-11 19:34:53 -0500

    Keybind

--- StartScene.gd
+++ StartScene.gd
@@ -8,12 +8,9 @@ func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var offsetVector = Vector2(OS.window_size.x, OS.window_size.y)
 	background.set_size(offsetVector)
