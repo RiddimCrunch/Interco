a7cebb291831fd86ae244b5ef90b70741f1003c5
Samuel Cossette <1957527@cshawi.ca>
2023-01-11 21:29:52 -0500

    Ajout enemy follow player

--- Enemy.gd
+++ Enemy.gd
@@ -1,12 +1,29 @@
 extends KinematicBody2D
 
-var speed = 10
+#var speed = 10
 var damage = 10
 
 var motion = Vector2.ZERO
 var screen_size = get_viewport_rect().size
 
 onready var player = get_node("res://Player.tscn") 
+
+enum {
+	Attack,
+	Walk
+}
+onready var Player = $"../../Player"
+var current_state = Walk
+export var speed = 350
+var path = []
+var cur_path_idx = 0
+var target = null
+var velocity = Vector2.ZERO
+var threshold = .1
+var turn_speed = 10
+
+onready var nav = get_parent()
+
 
 func move_to(_location):
 	var _target = _location
@@ -16,9 +33,28 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
+	if path.size() > 0:
+		move_to_target()
+		
+func _process(delta):
+	match current_state:
+		Attack:
+			pass
+		Walk:
+			$Look.look_at(Player.global_transform.origin)
+			rotate(deg2rad($Look.rotation * turn_speed))
+			
 
-	motion += position.direction_to(player.position)
+func move_to_target():
+	if global_transform.origin.distance_to(path[cur_path_idx]) < threshold:
+		path.remove(0)
+	else:
+		var direction = path[cur_path_idx] - global_transform.origin
+		velocity = direction.normalized() * speed
+		#print(velocity)
+		move_and_slide(velocity, Vector2.UP)
+		
+func get_target_path(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
 	
-	look_at(player.position)
 	
-	motion = move_and_slide(motion)
